d32b258b737d0c9ad6c679a5b3bed1de024ff5fd
eamobi <amobisomto@gmail.com>
2019-02-01 17:30:54 -0600

    clearBoard function not working

--- game.js
+++ game.js
@@ -1,10 +1,31 @@
+const MAX_TURNS = 9;
 var squares = document.getElementsByClassName('square')
-function playTurn(e) {
+function playTurnX(e) {
     var play = document.querySelector('#'+ this.id);
     console.log(this.id);
-    play.innerHTML = this.id;
+    play.innerHTML = 'X';
     // var input = document.querySelector('this.id')
 }
-for (let i = 0; i < squares.length; i++) {
-    squares[i].addEventListener('click', playTurn);
+function playTurnO(e) {
+    var play = document.querySelector('#'+ this.id);
+    console.log(this.id);
+    play.innerHTML ='O';
+    // var input = document.querySelector('this.id')
 }
+//Function that resets the board when the game is over 
+function clearBoard(arr){
+    for (let i = 0; i < arr.length; i++){
+        arr[i] = '';
+    }
+}
+//Game Loop 
+for (let i = 0; i < squares.length; i++) {
+    if (i % 2 == 0){
+        squares[i].addEventListener('click', playTurnX);
+    }
+    else{
+        squares[i].addEventListener('click', playTurnO);
+    }
+    
+}
+clearBoard(squares);
