e92aee514be0acaeda2583df2424aa37085e1c54
eamobi <amobisomto@gmail.com>
2019-02-01 14:27:41 -0600

    changed id names, because numbers were causing errors

--- game.js
+++ game.js
@@ -1,8 +1,10 @@
 var squares = document.getElementsByClassName('square')
-function playTurn(e){
-console.log(this.id);
+function playTurn(e) {
+    var play = document.querySelector('#'+ this.id);
+    console.log(this.id);
+    play.innerHTML = this.id;
     // var input = document.querySelector('this.id')
 }
-for (let i = 0; i < squares.length; i++){
-    squares[i].addEventListener('click',playTurn);
+for (let i = 0; i < squares.length; i++) {
+    squares[i].addEventListener('click', playTurn);
 }
