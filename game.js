b32c81fcb06fd1681f3594cb76e75ff48e8ea08d
eamobi <amobisomto@gmail.com>
2019-02-01 20:54:49 -0600

    checking checkWin method

--- game.js
+++ game.js
@@ -12,11 +12,13 @@ function playTurn(e){
     if (counter % 2 === 0){
         play.innerHTML = 'X';
         counter++;
+        console.log(checkWin());
 
     }
     else if (counter % 2 !== 0){
         play.innerHTML = 'O';
         counter++;
+        console.log(checkWin());
     }
 }
 //Function that resets the board 
@@ -24,6 +26,9 @@ function clearBoard(){
     for (let i = 0; i < squares.length; i++){
         squares[i].innerHTML = '';
     }
+}
+function checkWin(){
+    return (checkHorizontal() || checkVertical() || checkDiagonal());
 }
 function checker(a,b,c){
     return (a === b) && (b === c);
@@ -35,6 +40,19 @@ function checkHorizontal(){
             checker(pos[7].innerHTML,pos[8].innerHTML,pos[9].innerHTML)
     )
 }
+
+function checkVertical(){
+    return (checker(pos[1].innerHTML,pos[4].innerHTML,pos[7].innerHTML) ||
+            checker(pos[2].innerHTML,pos[5].innerHTML,pos[8].innerHTML)||
+            checker(pos[3].innerHTML,pos[6].innerHTML,pos[9].innerHTML)
+    )
+}
+function checkDiagonal(){
+    return (checker(pos[1].innerHTML,pos[5].innerHTML,pos[9].innerHTML) ||
+            checker(pos[3].innerHTML,pos[5].innerHTML,pos[7].innerHTML)
+    )
+}
+
 //Add event listener to each of the squares
 for (let i = 0; i < squares.length; i++) {
         squares[i].addEventListener('click', playTurn);
